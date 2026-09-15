605e9d89483ca94ea700454bd34645d039f02095
Andrii Pereverziev <pereverziev.andrii@gmail.com>
2026-09-15 23:02:11 +0300

    animation_2

--- src/animations/animation_2.c
+++ src/animations/animation_2.c
@@ -2,38 +2,51 @@
 #include <zephyr/display/cfb.h>
 #include <math.h>
 
-float xpos = 0.0f;
-float ypos = 0.0f;
-float zpos = 150.0f;
-int fl = 250;
-int vpX = 50;
-int vpY = 50;
-int radius = 20;
+#ifndef M_PI
+#define M_PI 3.14159265358979323846f
+#endif
 
-float angle = 0.0f;
+
+static float xpos = 0.0f;
+static float ypos = 0.0f;
+static float zpos = 350.0f;
+
+static const int fl = 250;
+static const int radius = 50;
+
+static float angle = 0.0f;
 
 void anim2_draw(const struct device *display, float dt, int width, int height, ControlsState buttons) {
-  vpX = width / 2;
-  vpY = height / 2;
+    int vpX = width / 2;
+    int vpY = height / 2;
 
-  if (buttons.btn_2) {
-    zpos += 500 * dt;
-  } else if (buttons.btn_4) {
-    zpos -= 500 * dt;
-  }
+    if (buttons.btn_2) {
+        zpos += 300.0f * dt;
+    } else if (buttons.btn_4) {
+        zpos -= 300.0f * dt;
+    }
 
-  float x = cosf(angle) * 100;
-  float y = sinf(angle) * 100;
+    if (zpos < 10.0f) {
+        zpos = 10.0f;
+    }
 
-  float scale = fl / (fl + zpos + y);
-  float scaled_radius = radius * scale;
+    float x = cosf(angle) * 100.0f;
+    float y = sinf(angle) * 100.0f;
 
-  struct cfb_position position = {
-    x: vpX + (xpos + x) * scale,
-    y: vpY + ypos * scale
-  };
+    float scale = (float)fl / ((float)fl + zpos + y);
+    
+    int16_t scaled_radius = (int16_t)(radius * scale);
+    if (scaled_radius < 1) {
+        scaled_radius = 1;
+    }
 
-  angle += 0.1f;
+    struct cfb_position position = {
+        .x = (int16_t)(vpX + (int)((xpos + x) * scale)),
+        .y = (int16_t)(vpY + (int)(ypos * scale))
+    };
 
-  cfb_draw_circle(display, &position, scaled_radius);
+    angle += 4.0f * dt;
+    if (angle > 4.0f * M_PI) {
+        angle -= 4.0f * M_PI;
+    }
 }
