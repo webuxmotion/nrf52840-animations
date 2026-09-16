5897beb064fbfe37e82acd9b59eabe4ec58ef9ec
Andrii Pereverziev <pereverziev.andrii@gmail.com>
2026-09-16 12:23:37 +0300

    refactor

--- src/animations/animation_2.c
+++ src/animations/animation_2.c
@@ -6,6 +6,8 @@
 #define M_PI 3.14159265358979323846f
 #endif
 
+#define DEG_TO_RAD(x) ((x) * M_PI / 180.0f)
+
 
 static float xpos = 0.0f;
 static float ypos = 0.0f;
@@ -59,148 +61,48 @@ void anim2_draw(const struct device *display, float dt, int width, int height, C
         .y = (int16_t)vpY
     };
 
-    float len1 = 51.193f;
-    float ang1 = 12.396f * M_PI / 180.0f;
+    typedef struct {
+      float length;
+      float angle;
+    } PolarVector;
 
-    float len2 = 54.471f;
-    float ang2 = -23.377 * M_PI / 180.0f;
-
-    float len3 = 60.578f;
-    float ang3 = -34.373 * M_PI / 180.0f;
-
-    float len4 = 83.501f;
-    float ang4 = -53.217 * M_PI / 180.0f;
-
-    float len5 = 42.249f;
-    float ang5 = -73.653 * M_PI / 180.0f;
-
-    float len6 = 74.812f;
-    float ang6 = -80.854 * M_PI / 180.0f;
-
-        // point 1
-    struct cfb_position v1 = {
-        .x = (int16_t)(cosf(ang1 + rotation) * len1 / scaleF),
-        .y = (int16_t)(sinf(ang1 + rotation) * len1 / scaleF)
-    };
-    struct cfb_position p1 = {
-        .x = (int16_t)(p0.x + v1.x),
-        .y = (int16_t)(p0.y + v1.y)
+    PolarVector polar_vectors[] = {
+      {51.193f,  DEG_TO_RAD(12.396f)},
+      {54.471f,  DEG_TO_RAD(-23.377f)},
+      {60.578f,  DEG_TO_RAD(-34.373f)},
+      {83.501f,  DEG_TO_RAD(-53.217f)},
+      {42.249f,  DEG_TO_RAD(-73.653f)},
+      {74.812f,  DEG_TO_RAD(-80.854f)}
     };
 
-    // point 2
-    struct cfb_position v2 = {
-        .x = (int16_t)(cosf(ang2 + rotation) * len2 / scaleF),
-        .y = (int16_t)(sinf(ang2 + rotation) * len2 / scaleF)
-    };
-    struct cfb_position p2 = {
-        .x = (int16_t)(p0.x + v2.x),
-        .y = (int16_t)(p0.y + v2.y)
-    };
+    typedef struct {
+      float x;
+      float y;
+    } Vector;
 
-    // point 3
-    struct cfb_position v3 = {
-        .x = (int16_t)(cosf(ang3 + rotation) * len3 / scaleF),
-        .y = (int16_t)(sinf(ang3 + rotation) * len3 / scaleF)
-    };
-    struct cfb_position p3 = {
-        .x = (int16_t)(p0.x + v3.x),
-        .y = (int16_t)(p0.y + v3.y)
-    };
+    Vector vectors[6];  
+    Vector mirrored_vectors[6];
+    struct cfb_position positions[6];
+    struct cfb_position mirrored_positions[6];
 
-    // point 4
-    struct cfb_position v4 = {
-        .x = (int16_t)(cosf(ang4 + rotation) * len4 / scaleF),
-        .y = (int16_t)(sinf(ang4 + rotation) * len4 / scaleF)
-    };
-    struct cfb_position p4 = {
-        .x = (int16_t)(p0.x + v4.x),
-        .y = (int16_t)(p0.y + v4.y)
-    };
+    for (int i = 0; i < 6; i++) {
+      vectors[i].x = (int16_t)(cosf(polar_vectors[i].angle + rotation) * polar_vectors[i].length / scaleF);
+      vectors[i].y = (int16_t)(sinf(polar_vectors[i].angle + rotation) * polar_vectors[i].length / scaleF);
+      mirrored_vectors[i].x = -vectors[i].x;
+      mirrored_vectors[i].y = -vectors[i].y;
+      positions[i].x = (int16_t)(p0.x + vectors[i].x);
+      positions[i].y = (int16_t)(p0.y + vectors[i].y);
+      mirrored_positions[i].x = (int16_t)(p0.x + mirrored_vectors[i].x);
+      mirrored_positions[i].y = (int16_t)(p0.y + mirrored_vectors[i].y);
+    }
 
-    // point 5
-    struct cfb_position v5 = {
-        .x = (int16_t)(cosf(ang5 + rotation) * len5 / scaleF),
-        .y = (int16_t)(sinf(ang5 + rotation) * len5 / scaleF)
-    };
-    struct cfb_position p5 = {
-        .x = (int16_t)(p0.x + v5.x),
-        .y = (int16_t)(p0.y + v5.y)
-    };
-
-    // point 6
-    struct cfb_position v6 = {
-        .x = (int16_t)(cosf(ang6 + rotation) * len6 / scaleF),
-        .y = (int16_t)(sinf(ang6 + rotation) * len6 / scaleF)
-    };
-    struct cfb_position p6 = {
-        .x = (int16_t)(p0.x + v6.x),
-        .y = (int16_t)(p0.y + v6.y)
-    };
-
-    struct cfb_position v1m = {
-        .x = -v1.x,
-        .y = -v1.y
-    };
-    struct cfb_position v2m = {
-        .x = -v2.x,
-        .y = -v2.y
-    };
-    struct cfb_position v3m = {
-        .x = -v3.x,
-        .y = -v3.y
-    };
-    struct cfb_position v4m = {
-        .x = -v4.x,
-        .y = -v4.y
-    };
-    struct cfb_position v5m = {
-        .x = -v5.x,
-        .y = -v5.y
-    };
-    struct cfb_position v6m = {
-        .x = -v6.x,
-        .y = -v6.y
-    };
-
-    struct cfb_position p1m = {
-        .x = p0.x + v1m.x,
-        .y = p0.y + v1m.y
-    };
-    struct cfb_position p2m = {
-        .x = p0.x + v2m.x,
-        .y = p0.y + v2m.y
-    };
-    struct cfb_position p3m = {
-        .x = p0.x + v3m.x,
-        .y = p0.y + v3m.y
-    };
-    struct cfb_position p4m = {
-        .x = p0.x + v4m.x,
-        .y = p0.y + v4m.y
-    };
-    struct cfb_position p5m = {
-        .x = p0.x + v5m.x,
-        .y = p0.y + v5m.y
-    };
-    struct cfb_position p6m = {
-        .x = p0.x + v6m.x,
-        .y = p0.y + v6m.y
-    };
-
-    cfb_draw_line(display, &p1, &p2);
-    cfb_draw_line(display, &p3, &p4);
-    cfb_draw_line(display, &p5, &p6);
-
-    cfb_draw_line(display, &p1m, &p2m);
-    cfb_draw_line(display, &p3m, &p4m);
-    cfb_draw_line(display, &p5m, &p6m);
-
-    cfb_draw_line(display, &p1, &p6m);
-    cfb_draw_line(display, &p2, &p5m);
-    cfb_draw_line(display, &p3, &p4m);
-    cfb_draw_line(display, &p4, &p3m);
-    cfb_draw_line(display, &p5, &p2m);
-    cfb_draw_line(display, &p6, &p1m);
+    for (int i = 0; i < 6; i++) {
+      cfb_draw_line(display, &positions[i], &mirrored_positions[5 - i]);
+      if (i % 2 == 0) {
+        cfb_draw_line(display, &positions[i], &positions[i + 1]);
+        cfb_draw_line(display, &mirrored_positions[i], &mirrored_positions[i + 1]);
+      }
+    }
 
     if (buttons.btn_3) {
       rotation += 0.1f;
